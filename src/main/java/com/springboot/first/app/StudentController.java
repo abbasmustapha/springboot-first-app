bf418cb848fac71846f6220a4e2eb5a92a20389a
abbas.m <zaz@zeze.com>
2021-10-16 22:33:11 +0200

    P2 rest api returns student list

--- src/main/java/com/springboot/first/app/StudentController.java
+++ src/main/java/com/springboot/first/app/StudentController.java
@@ -3,11 +3,26 @@ package com.springboot.first.app;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.*;
+
 @RestController
 public class StudentController {
 
     @GetMapping("/student")
     public Student getStudent(){
         return new Student("James","Bond");
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+
+        students.add(new Student("Steeve","McQueen"));
+        students.add(new Student("John","Wagne"));
+        students.add(new Student("Paul","Bismuth"));
+        students.add(new Student("Jack","London"));
+
+        return students;
+
     }
 }
